3a7267afd5424a0aa5ed791791d6546b173f33b3
Tulikasharma <tulikasharma368@gmail.com>
2021-09-29 10:52:37 +0530

    [Tulika] update: conversion working for both sides

--- js/temp_functionality.js
+++ js/temp_functionality.js
@@ -45,3 +45,51 @@ function convertTemp(){
             break;
     }
 }
+
+function convertTempBack(){
+    var input = document.querySelector("#value2").value;
+    var selectVal = document.querySelector('#selectTo').value;
+    var selectVal2 = document.querySelector('#selectFrom').value;
+    const result = document.querySelector('#value1');
+    switch (selectVal) {
+        case "celcius":
+            switch (selectVal2) {
+                case "celcius":
+                    result.value = input;
+                    break;
+                case "fahrenheit":
+                    result.value = (input * 1.8) + 32;
+                    break;
+                case "kelvin":
+                    result.value = input +273.15;
+                    break;
+            }
+            break;
+        case "fahrenheit":
+            switch (selectVal2) {
+                case "celcius":
+                    result.value = (input -32)/1.8;
+                    break;
+                case "fahrenheit":
+                    result.value = input;
+                    break;
+                case "kelvin":
+                    result.value = (input - 32) * (5/9) +273.15;
+                    break;
+            }
+            break;
+        case "kelvin":
+            switch (selectVal2) {
+                case "celcius":
+                    result.value = input - 273.15;
+                    break;
+                case "fahrenheit":
+                    result.value = (input - 273.15) * (9/5) +32;
+                    break;
+                case "kelvin":
+                    result.value = input;
+                    break;
+           }
+            break;
+    }
+}
--- js/vol_functionality.js
+++ js/vol_functionality.js
@@ -45,3 +45,51 @@ function convertVol(){
             break;
     }
 }
+
+function convertVolBack(){
+    var input = document.querySelector("#value2").value;
+    var selectVal = document.querySelector('#selectTo').value;
+    var selectVal2 = document.querySelector('#selectFrom').value;
+    const result = document.querySelector('#value1');
+    switch (selectVal) {
+        case "litre":
+            switch (selectVal2) {
+                case "litre":
+                    result.value = input;
+                    break;
+                case "millileter":
+                    result.value = input * 1000;
+                    break;
+                case "gallon":
+                    result.value = input * 0.264;
+                    break;
+            }
+            break;
+        case "millileter":
+            switch (selectVal2) {
+                case "litre":
+                    result.value = input * 0.001;
+                    break;
+                case "millimetre":
+                    result.value = input;
+                    break;
+                case "gallon":
+                    result.value = input * 0.000264;
+                    break;
+            }
+            break;
+        case "gallon":
+            switch (selectVal2) {
+                case "litre":
+                    result.value = input * 3.785;
+                    break;
+                case "millileter":
+                    result.value = input * 3785411.78;
+                    break;
+                case "gallon":
+                    result.value = input;
+                    break;
+           }
+            break;
+    }
+}
